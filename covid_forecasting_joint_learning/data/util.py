c9e644af540dbb2a14443399102ccff6d4fa4f9f
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2021-06-16 20:34:26 +0700

    Forgot to save this when cleaning up

--- covid_forecasting_joint_learning/data/util.py
+++ covid_forecasting_joint_learning/data/util.py
@@ -37,18 +37,3 @@ def right_slice(
 ):
     return left[right.first_valid_index():right.last_valid_index()]
 
-
-def set_tuple(a):
-    if isinstance(a, list):
-        return tuple(set([set_tuple(x) for x in a]))
-    return a
-
-
-def set_similarity(a, b):
-    return len(a.intersection(b))/len(a.union(b))
-
-
-def find_similar_set(a, sets):
-    similarities = [(b, set_similarity(a, b)) for b in sets]
-    return max(similarities, key=lambda x: x[1])
-
