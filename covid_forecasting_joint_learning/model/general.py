5212f7e2ff62e47803a3a7be2129c5df05f8ee5d
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2021-06-29 21:25:11 +0700

    Past no longer care about exo

--- covid_forecasting_joint_learning/model/general.py
+++ covid_forecasting_joint_learning/model/general.py
@@ -401,7 +401,7 @@ class ObjectiveModel:
 
         use_exo_cols = future_exo_cols is not None and len(future_exo_cols) > 0
         if use_exo_cols:
-            input_size_past += sample["past_exo"].shape[-1]
+            # input_size_past += sample["past_exo"].shape[-1]
             input_size_future += sample["future_exo"].shape[-1]
         model_kwargs["use_exo"] = use_exo_cols
 
