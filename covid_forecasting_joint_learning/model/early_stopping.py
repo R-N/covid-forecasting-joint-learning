6463596e791513c8353c466ddd2ccdcb25ca168f
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2021-07-27 23:56:02 +0700

    Fix inconsistent indentation

--- covid_forecasting_joint_learning/model/early_stopping.py
+++ covid_forecasting_joint_learning/model/early_stopping.py
@@ -19,38 +19,38 @@ class EarlyStopping:
 
     def __call__(self, train_loss, val_loss):
         if self.best_val_loss == None:
-        	self.update_best(train_loss, val_loss)
+            self.update_best(train_loss, val_loss)
         else:
-        	delta_val_loss = self.best_loss - val_loss
-        	rise = delta_val_loss < self.min_delta
-	        if rise:
-	            self.rise_counter += 1
-	            if self.debug:
-	            	print(f"INFO: Early stopping rise {self.rise_counter}/{self.rise_patience}")
-	            if self.rise_counter >= self.rise_patience:
-            		self.early_stop("rise")
-	        else:
-	            self.rise_counter = 0
-	            still = abs(delta_val_loss) < self.min_delta
-	            if val_loss < self.best_val_loss or (still and train_loss < self.best_train_loss):
-	            	self.update_best(train_loss, val_loss)
-	            if still:
-	            	self.still_counter += 1
-		            if self.debug:
-		            	print(f"INFO: Early stopping still {self.still_counter}/{self.still_patience}")
-	            	if self.still_counter >= self.still_patience:
-	            		self.early_stop("still")
-            	else:
-            		self.still_counter = 0
-		return self.early_stopped
+            delta_val_loss = self.best_loss - val_loss
+            rise = delta_val_loss < self.min_delta
+            if rise:
+                self.rise_counter += 1
+                if self.debug:
+                    print(f"INFO: Early stopping rise {self.rise_counter}/{self.rise_patience}")
+                if self.rise_counter >= self.rise_patience:
+                    self.early_stop("rise")
+            else:
+                self.rise_counter = 0
+                still = abs(delta_val_loss) < self.min_delta
+                if val_loss < self.best_val_loss or (still and train_loss < self.best_train_loss):
+                    self.update_best(train_loss, val_loss)
+                if still:
+                    self.still_counter += 1
+                    if self.debug:
+                        print(f"INFO: Early stopping still {self.still_counter}/{self.still_patience}")
+                    if self.still_counter >= self.still_patience:
+                        self.early_stop("still")
+                else:
+                    self.still_counter = 0
+        return self.early_stopped
 
-	def update_best(self, train_loss, val_loss):
-		self.best_train_loss = train_loss
+    def update_best(self, train_loss, val_loss):
+        self.best_train_loss = train_loss
         self.best_val_loss = val_loss
         self.best_state = self.model.state_dict()
 
-	def early_stop(self, reason="idk"):
-    	self.model.load_state_dict(self.best_state)
+    def early_stop(self, reason="idk"):
+        self.model.load_state_dict(self.best_state)
         self.early_stopped = True
         if self.debug:
-    		print(f"INFO: Early stopping due to {reason}")
+            print(f"INFO: Early stopping due to {reason}")
