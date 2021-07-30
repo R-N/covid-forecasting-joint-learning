d47d95f3cb03a7f33723a69ad7b4ab3479eeef04
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2021-07-30 14:47:51 +0700

    Forgot to remove this

--- covid_forecasting_joint_learning/model/__init__.py
+++ covid_forecasting_joint_learning/model/__init__.py
@@ -1,5 +1,5 @@
 from .train import train, test
 from .modules.main import SingleModel
 from .general import ClusterModel, ObjectiveModel
-from .early_stopping import EarlyStopping, EarlyStopping2
+from .early_stopping import EarlyStopping
 from . import util as ModelUtil
