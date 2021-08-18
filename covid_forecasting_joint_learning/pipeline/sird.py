676b4f3c4bf9ea6f98c6c9173724a8822bc70a20
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2021-08-18 23:38:45 +0700

    Update sird.py

--- covid_forecasting_joint_learning/pipeline/sird.py
+++ covid_forecasting_joint_learning/pipeline/sird.py
@@ -2,26 +2,11 @@ import pandas as pd
 from ..data import cols as DataCol
 
 
-def calc_s(df, n):
-    df[DataCol.N] = pd.Series(n, index=df.index)
-    df[DataCol.S] = df[DataCol.N] - (df[DataCol.I] + df[DataCol.R] + df[DataCol.D])
-    return df
-
-
 def calc_s_global(df, n_global):
     df[DataCol.N_GLOBAL] = pd.Series(n_global, index=df.index)
     df[DataCol.S_GLOBAL_PEOPLE] = df[DataCol.N_GLOBAL] - (df[DataCol.I_TOT_GLOBAL] + df[DataCol.VAC_PEOPLE])
     df[DataCol.S_GLOBAL_FULL] = df[DataCol.N_GLOBAL] - (df[DataCol.I_TOT_GLOBAL] + df[DataCol.VAC_FULL])
     df[DataCol.POS_RATE] = df[DataCol.I_TOT_GLOBAL] / df[DataCol.TEST]
-    return df
-
-
-def calc_delta(df, df_shifted=None):
-    df_shifted = df.shift() if df_shifted is None else df_shifted
-
-    df[DataCol.DELTA_R] = df[DataCol.R] - df_shifted[DataCol.R]
-    df[DataCol.DELTA_D] = df[DataCol.D] - df_shifted[DataCol.D]
-    df[DataCol.DELTA_I] = df[DataCol.I] - df_shifted[DataCol.I]
     return df
 
 
@@ -35,6 +20,31 @@ def calc_delta_global(df, df_shifted=None):
     return df
 
 
+def calc_vars_global(df, df_shifted=None):
+    df_shifted = df.shift() if df_shifted is None else df_shifted
+
+    df[DataCol.VAC_PEOPLE_S] = df[DataCol.DELTA_VAC_PEOPLE] / df_shifted[DataCol.S_GLOBAL_PEOPLE]
+    df[DataCol.VAC_FULL_S] = df[DataCol.DELTA_VAC_FULL] / df_shifted[DataCol.S_GLOBAL_FULL]
+    df[DataCol.DAILY_POS_RATE] = df[DataCol.DELTA_I_TOT_GLOBAL] / df[DataCol.DELTA_TEST]
+
+    return df
+
+
+def calc_s(df, n):
+    df[DataCol.N] = pd.Series(n, index=df.index)
+    df[DataCol.S] = df[DataCol.N] - (df[DataCol.I] + df[DataCol.R] + df[DataCol.D])
+    return df
+
+
+def calc_delta(df, df_shifted=None):
+    df_shifted = df.shift() if df_shifted is None else df_shifted
+
+    df[DataCol.DELTA_R] = df[DataCol.R] - df_shifted[DataCol.R]
+    df[DataCol.DELTA_D] = df[DataCol.D] - df_shifted[DataCol.D]
+    df[DataCol.DELTA_I] = df[DataCol.I] - df_shifted[DataCol.I]
+    return df
+
+
 def calc_vars(df, n, df_shifted=None):
     df_shifted = df.shift() if df_shifted is None else df_shifted
     df[DataCol.BETA] = n * (
@@ -45,15 +55,6 @@ def calc_vars(df, n, df_shifted=None):
 
     return df
 
-
-def calc_vars_global(df, df_shifted=None):
-    df_shifted = df.shift() if df_shifted is None else df_shifted
-
-    df[DataCol.VAC_PEOPLE_S] = df[DataCol.DELTA_VAC_PEOPLE] / df_shifted[DataCol.S_GLOBAL_PEOPLE]
-    df[DataCol.VAC_FULL_S] = df[DataCol.DELTA_VAC_FULL] / df_shifted[DataCol.S_GLOBAL_FULL]
-    df[DataCol.DAILY_POS_RATE] = df[DataCol.DELTA_I_TOT_GLOBAL] / df[DataCol.DELTA_TEST]
-
-    return df
 
 def rebuild(sird_vars, prev, n, index=None):
     is_df = isinstance(sird_vars, pd.DataFrame)
