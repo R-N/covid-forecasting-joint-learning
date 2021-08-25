c17cb28d2f62b5fd7f2c7817860dfa902e3cf2fc
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2021-08-25 19:01:17 +0700

    Avoid dataframe fragmentation

--- covid_forecasting_joint_learning/data/util.py
+++ covid_forecasting_joint_learning/data/util.py
@@ -26,12 +26,15 @@ def write_string(s, path):
 
 
 def add_dates(df, ranges, name):
+    df = df.copy()
     ranges = ranges.itertuples(index=False) if isinstance(ranges, pd.DataFrame) else ranges
     df.loc[:, name] = pd.Series(0.0, index=df.index, dtype=DEFAULT_DTYPE)
     for start, end, value in ranges:
         # start, end = pd.to_datetime((start, end))
         df.loc[start:end, name] = value
-    return df
+    ret = df.copy()
+    del df
+    return ret
 
 def prepare_dates(
     df,
