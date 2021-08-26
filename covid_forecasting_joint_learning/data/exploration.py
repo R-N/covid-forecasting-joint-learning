210c1a99d6c10f9da561aaac69081b1a21897595
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2021-08-27 01:58:25 +0700

    Update exploration.py

--- covid_forecasting_joint_learning/data/exploration.py
+++ covid_forecasting_joint_learning/data/exploration.py
@@ -274,7 +274,7 @@ def corr_lag_best_multi(
         lag_start=lag_start,
         lag_end=lag_end
     ), key=lambda x: abs(x)) for y_col in y_cols] for x_col in x_cols])
-    print(df[x_cols + y_cols])
+    print(df[x_cols + y_cols].isnull().sum())
     print(corr)
     if reduction:
         if abs_corr:
