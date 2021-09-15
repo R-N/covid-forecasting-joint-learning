76e26829519be5f6b519af40bdc06f157684097a
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2021-09-15 15:07:24 +0700

    Update general.py

--- covid_forecasting_joint_learning/model/general.py
+++ covid_forecasting_joint_learning/model/general.py
@@ -864,7 +864,7 @@ def make_objective(
 
                 if pretrain_copy:
                     if model_dir_copy_i:
-                        ModelUtil.copytree(model_dir, model_dir_copy_i, dirs_exist_ok=True)
+                        ModelUtil.copytree(model_dir_i, model_dir_copy_i, dirs_exist_ok=True)
                 if drive and pretrain_upload:
                     upload_logs(drive, trial_id, log_dir_i, log_dir_id, model_dir_i, model_dir_id)
 
